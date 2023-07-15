225e41bb790be6597205ac90c8ef65a0751ff8c7
Hajra Khatri <khatrihajra173@gmail.com>
2023-07-15 20:48:36 +0530

    Theme Changer HTML CSS JS

---
+++ script.js
@@ -0,0 +1,28 @@
+function changeColor(color) {
+    document.body.style.background = color;
+}
+ 
+function changeToBlue() {
+    changeColor('blue');
+    document.body.style.color ='white' ;
+
+}	
+function changeToBlack() {
+    changeColor('black');
+    document.body.style.color ='white' ;
+
+}	
+function changeToWhite() {
+    changeColor('white');
+    document.body.style.color ='black' ;
+}	
+function changeToGrey() {
+    changeColor('grey');
+    document.body.style.color ='black' ;
+
+}
+	function changeToBrown() {
+    changeColor('brown');
+    document.body.style.color ='white' ;
+
+}	
